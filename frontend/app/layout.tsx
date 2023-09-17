d1eeb9bde93bdb9b67f76b4687259eab9bce49ae
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-09-17 20:41:29 -0300

    feat: create frontend next.js app

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Header from './shared/header'
+import { Children } from './shared/types'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'CM42 Media Critic',
+  description: 'An platform for media (movies, books, games) critics',
+}
+
+export default function RootLayout({ children }: Children) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#B53535",
+        secondary: "#841717",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
