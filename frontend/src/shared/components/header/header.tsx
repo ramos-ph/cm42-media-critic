9fab8669b96604ef536c64b3e8b2b23a545940ab
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-09-22 11:25:50 -0300

    feat: add mobile responsiveness

--- frontend/src/shared/components/header/header.tsx
+++ frontend/src/shared/components/header/header.tsx
@@ -3,8 +3,8 @@ import Search from '../search/search'
 
 export default function Header() {
   return (
-    <header className='p-8 bg-primary flex justify-between items-center relative bg-cm42'>
-      <Image className='ml-20' src='/codeminer-main-logo.svg' alt='CM42 Media Critic, white logo' width={376} height={80} />
+    <header className='p-8 bg-primary flex flex-col md:flex-row gap-4 md:justify-between items-center relative bg-cm42'>
+      <Image className='md:ml-20' src='/codeminer-main-logo.svg' alt='CM42 Media Critic, white logo' width={376} height={80} />
       <Search />
     </header>
   )
--- frontend/src/shared/components/search/search.tsx
+++ frontend/src/shared/components/search/search.tsx
@@ -1,7 +1,7 @@
 export default function Search() {
   return (
     <input
-      className="p-4 rounded-2xl bg-secondary text-white placeholder:text-gray-300 outline-white w-[496px] h-fit"
+      className="p-4 rounded-2xl bg-secondary text-white placeholder:text-gray-300 shadow-lg shadow-primary outline-white md:w-[496px] w-full h-fit"
       type="text"
       placeholder="Search media..."
     />
