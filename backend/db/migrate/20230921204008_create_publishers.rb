21a9aadb330153a7d623c354cdec09d4b6c67b9e
ramos-ph <pedro.henrique@codeminer42.com>
2023-09-26 09:03:53 -0300

    Undo migrations

--- backend/db/migrate/20230921204008_create_publishers.rb
+++
@@ -1,9 +0,0 @@
-class CreatePublishers < ActiveRecord::Migration[7.0]
-  def change
-    create_table :publishers do |t|
-      t.string :name, null: false
-
-      t.timestamps
-    end
-  end
-end
--- backend/db/migrate/20230921204042_create_platforms.rb
+++
@@ -1,9 +0,0 @@
-class CreatePlatforms < ActiveRecord::Migration[7.0]
-  def change
-    create_table :platforms do |t|
-      t.string :name, null: false
-
-      t.timestamps
-    end
-  end
-end
