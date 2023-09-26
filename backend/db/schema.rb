18732b1bb2834e9a605429e84eb79dcb726aa78a
ramos-ph <pedro.henrique@codeminer42.com>
2023-09-26 09:34:57 -0300

    Add CreateGennres migration

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_26_122253) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_26_122354) do
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "platforms", force: :cascade do |t|
     t.string "name", null: false
     t.string "description", null: false
