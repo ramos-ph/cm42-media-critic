2460168fc7a305fa9ecad3bf73e303acfc416758
ramos-ph <pedro.henrique@codeminer42.com>
2023-09-26 11:13:04 -0300

    Fix SoftwareHouse foreign keys definition

--- backend/app/models/software_house.rb
+++ backend/app/models/software_house.rb
@@ -4,10 +4,10 @@ class SoftwareHouse < ApplicationRecord
   has_and_belongs_to_many :developed_games,
                           join_table: "games_developers",
                           class_name: "Game",
-                          foreign_key: "developer_id"
+                          foreign_key: "software_house_id"
 
   has_and_belongs_to_many :published_games,
                           join_table: "games_publishers",
                           class_name: "Game",
-                          foreign_key: "publisher_id"
+                          foreign_key: "software_house_id"
 end
