fa8ca75c7d04f6f9768134fa7aac30e64716b529
ramos-ph <pedro.henrique@codeminer42.com>
2023-09-26 11:06:20 -0300

    Fix Game-SoftwareHouse relationships

--- backend/app/models/game.rb
+++ backend/app/models/game.rb
@@ -1,16 +1,16 @@
 class Game < ApplicationRecord
   validates_presence_of :title, :description, :year
 
-  has_and_belongs_to_many :platforms,        join_table: "games_platforms"
-  has_and_belongs_to_many :genres,           join_table: "games_genres"
+  has_and_belongs_to_many :platforms, join_table: "games_platforms"
+  has_and_belongs_to_many :genres,    join_table: "games_genres"
 
-  has_and_belongs_to_many :games_developers,
+  has_and_belongs_to_many :developers,
                           join_table: "games_developers",
-                          class_name: "SofwareHouse",
+                          class_name: "SoftwareHouse",
                           foreign_key: "game_id"
 
-  has_and_belongs_to_many :games_publishers,
+  has_and_belongs_to_many :publishers,
                           join_table: "games_publishers",
-                          class_name: "SofwareHouse",
+                          class_name: "SoftwareHouse",
                           foreign_key: "game_id"
 end
--- backend/app/models/software_house.rb
+++ backend/app/models/software_house.rb
@@ -1,12 +1,12 @@
 class SoftwareHouse < ApplicationRecord
   validates_presence_of :name, :description
 
-  has_and_belongs_to_many :games_developers,
+  has_and_belongs_to_many :developed_games,
                           join_table: "games_developers",
                           class_name: "Game",
                           foreign_key: "developer_id"
 
-  has_and_belongs_to_many :games_publishers,
+  has_and_belongs_to_many :published_games,
                           join_table: "games_publishers",
                           class_name: "Game",
                           foreign_key: "publisher_id"
