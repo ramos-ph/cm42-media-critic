2a11bd7d844e4b3ab3467f47d7a7ceea5a52d21e
ramos-ph <pedro.henrique@codeminer42.com>
2023-09-27 11:01:41 -0300

    Implement GamesController#index specs

--- backend/spec/requests/games_spec.rb
+++ backend/spec/requests/games_spec.rb
@@ -1,20 +1,31 @@
 require "rails_helper"
 
 RSpec.describe GamesController, type: :request do
-  let(:json_body) { JSON.parse(response.body) }
+  let(:response_json) { JSON.parse(response.body) }
 
   describe "#index" do
     context "when there are no games" do
       it "responds with an empty array" do
         get "/api/games"
 
-        expect(json_body).to eq([])
+        expect(response_json).to eq([])
         expect(response).to have_http_status(:ok)
       end
     end
 
     context "when there are games" do
-      it "responds with an array of games"
+      let!(:game) { Game.create(title: "Stardew Valley", description: "The best game", year: 2016) }
+
+      it "responds with an array of games" do
+        get "/api/games"
+
+        expect(response_json.first).to include({
+          "title" => "Stardew Valley",
+          "description" => "The best game",
+          "year" => 2016
+        })
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 
