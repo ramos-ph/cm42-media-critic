c997edc095a22167cc5b3c8583d16753df8f8f23
ramos-ph <pedro.henrique@codeminer42.com>
2023-09-27 10:30:16 -0300

    Structure HealthController spec

--- backend/spec/requests/health_spec.rb
+++ backend/spec/requests/health_spec.rb
@@ -1,10 +1,12 @@
 require "rails_helper"
 
-RSpec.describe "Health", type: :request do
-  it "responds with 'ok'" do
-    get "/api/health"
+RSpec.describe HealthController, type: :request do
+  describe "#index" do
+    it "responds with 'ok'" do
+      get "/api/health"
 
-    expect(response).to have_http_status(:ok)
-    expect(response.body).to include("ok")
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include("ok")
+    end
   end
 end
