a8a0b345c0182f78703b794a0570a801ff5fa97e
ramos-ph <pedro.henrique@codeminer42.com>
2023-09-27 11:19:25 -0300

    Implement GamesController#show spec

--- backend/spec/requests/games_spec.rb
+++ backend/spec/requests/games_spec.rb
@@ -31,11 +31,26 @@ RSpec.describe GamesController, type: :request do
 
   describe "#show" do
     context "when game is not found" do
-      it "responds with 404"
+      it "responds with 404" do
+        get "/api/games/1"
+
+        expect(response).to have_http_status(:not_found)
+      end
     end
 
     context "when game is found" do
-      it "responds with the game"
+      let!(:game) { Game.create(title: "Stardew Valley", description: "The best game", year: 2016) }
+
+      it "responds with the game" do
+        get "/api/games/#{game.id}"
+
+        expect(response_json).to include({
+          "title" => "Stardew Valley",
+          "description" => "The best game",
+          "year" => 2016
+        })
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 
