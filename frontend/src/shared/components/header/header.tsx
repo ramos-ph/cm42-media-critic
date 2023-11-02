ea1181f9310faedecaa952c22b76132ab2e0fad4
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-11-02 09:53:19 -0300

    feat: add root link shared/header

--- frontend/src/shared/components/header/header.tsx
+++ frontend/src/shared/components/header/header.tsx
@@ -1,10 +1,19 @@
 import Image from 'next/image'
 import Search from '../search/search'
+import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className='p-8 bg-primary flex flex-col md:flex-row gap-4 md:justify-between items-center relative bg-cm42'>
-      <Image className='md:ml-20' src='/codeminer-main-logo.svg' alt='CM42 Media Critic, white logo' width={376} height={80} />
+    <header className="p-8 bg-primary flex flex-col md:flex-row gap-4 md:justify-between items-center relative bg-cm42">
+      <Link href="/">
+        <Image
+          className="md:ml-20"
+          src="/codeminer-main-logo.svg"
+          alt="CM42 Media Critic, white logo"
+          width={376}
+          height={80}
+        />
+      </Link>
       <Search />
     </header>
   )
