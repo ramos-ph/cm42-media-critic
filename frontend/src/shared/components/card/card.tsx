46b02a7261dba282007fb834d09cce9f25e83919
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-11-02 09:52:30 -0300

    feat: update shared/card component

--- frontend/src/shared/components/card/card.tsx
+++ frontend/src/shared/components/card/card.tsx
@@ -6,7 +6,7 @@ type Props = Children & { className?: string }
 export default function Card({ children, className }: Props) {
 
   return (
-    <div className={classNames("rounded-lg bg-tertiary p-8 border-primary", className)}>
+    <div className={classNames("rounded-lg bg-tertiary p-8", className)}>
       {children}
     </div>
   )
