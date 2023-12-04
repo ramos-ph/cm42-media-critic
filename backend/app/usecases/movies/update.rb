b5d30207819f2d08e2a83fa41bd199280a55d481
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-12-04 09:36:17 -0300

    feat: implement update movie use case

---
+++ backend/app/usecases/movies/update.rb
@@ -0,0 +1,22 @@
+class Movies::Update
+  def initialize(params:, movie: Movie, repository: MoviesRepository)
+    @params = params
+    @movie = movie
+    @repository = repository
+  end
+
+  def call
+    movie = @movie.new(
+      director: @params[:director],
+      writer: @params[:writer],
+      title: @params[:title],
+      producer: @params[:producer],
+      production_company: @params[:production_company],
+      cast: @params[:cast],
+      year: @params[:year]
+    )
+
+    current_movie = @repository.find(@params[:id])
+    current_movie.update!(movie.to_hash)
+  end
+end
